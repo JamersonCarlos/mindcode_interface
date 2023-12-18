205504c7f7bbb9947b3109dc1389de6938a916eb
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-12-18 04:14:13 -0300

    development of the toolbar, and the list of child directories

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'dart:io' as io;
 
 class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-
+  const MainPage({super.key, required this.path, required this.listDirectory});
+  final String path;
+  final List<io.FileSystemEntity> listDirectory;
   @override
   State<MainPage> createState() => _MainPageState();
 }
@@ -11,7 +13,145 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            width: 300,
+            decoration: const BoxDecoration(
+              border: Border(
+                right: BorderSide(
+                  width: 1,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: widget.listDirectory.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 7),
+                  child: Container(
+                    width: 120,
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      width: 1,
+                      color: Colors.black,
+                    )),
+                    child: Center(
+                      child: Text(
+                          '${widget.listDirectory[index].path.replaceAll('\\', '/').split('/').last}'),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Container(
+                  height: 50,
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        width: 1,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffd9d9d9),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.black,
+                            ),
+                          ),
+                          child: const Center(
+                            child: Text('Problemas de código'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffd9d9d9),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.black,
+                            ),
+                          ),
+                          child: const Center(
+                            child: Text('Overview'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffd9d9d9),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.black,
+                            ),
+                          ),
+                          child: const Center(
+                            child: Text('Funcionalidades'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffd9d9d9),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.black,
+                            ),
+                          ),
+                          child: const Center(
+                            child: Text('Arquitetura'),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Container(
+                          width: 200,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffd9d9d9),
+                            border: Border.all(
+                              width: 1,
+                              color: Colors.black,
+                            ),
+                          ),
+                          child: const Center(
+                            child: Text('Requisitos Técnicos'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
