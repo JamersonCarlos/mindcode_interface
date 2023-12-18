dec2042d8fcfd6b9fdaded866234efd3c902766c
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-12-18 04:15:09 -0300

    ​ 36 / 5.000 Resultados de tradução Resultado da tradução method to list child directories

--- lib/services/check_directory_service.dart
+++ lib/services/check_directory_service.dart
@@ -4,4 +4,10 @@ class ExistsDirectoryService {
   Future<bool> checkDirectory(String path) {
     return io.Directory(path).exists();
   }
+
+  List<io.FileSystemEntity> listDirectory(String path) {
+    List<io.FileSystemEntity> list = io.Directory(path).listSync();
+    list.removeWhere((element) => element.uri.toString().contains('.'));
+    return list;
+  }
 }
