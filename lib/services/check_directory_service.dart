84d10ee24c1e9b59097278dd0670ae686399e908
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-12-18 19:18:07 -0300

    ​ 59 / 5.000 Resultados de tradução Resultado da tradução adjust the way directories and files are displayed
    sorting the directories in alphabetical order, and leaving the files last in the list of files/directories

--- lib/services/check_directory_service.dart
+++ lib/services/check_directory_service.dart
@@ -7,7 +7,37 @@ class ExistsDirectoryService {
 
   List<io.FileSystemEntity> listDirectory(String path) {
     List<io.FileSystemEntity> list = io.Directory(path).listSync();
-    list.removeWhere((element) => element.uri.toString().contains('.'));
-    return list;
+    List<io.FileSystemEntity> listDirectory = [];
+    List<io.FileSystemEntity> listFile = [];
+    for (int i = 0; i < list.length; i++) {
+      if (list[i].runtimeType.toString()[1] == "D") {
+        listDirectory.add(list[i]);
+      } else {
+        listFile.add(list[i]);
+      }
+    }
+    listDirectory.sort(
+      (a, b) {
+        String firstAtribut = a.path.replaceAll('\\', '/').split('/').last;
+        String secondAtribut = b.path.replaceAll('\\', '/').split('/').last;
+        int comparacao = firstAtribut.compareTo(secondAtribut);
+        if (comparacao != 0) {
+          return comparacao;
+        }
+        return firstAtribut.compareTo(secondAtribut);
+      },
+    );
+    listFile.sort(
+      (a, b) {
+        String firstAtribut = a.path.replaceAll('\\', '/').split('/').last;
+        String secondAtribut = b.path.replaceAll('\\', '/').split('/').last;
+        int comparacao = firstAtribut.compareTo(secondAtribut);
+        if (comparacao != 0) {
+          return comparacao;
+        }
+        return firstAtribut.compareTo(secondAtribut);
+      },
+    );
+    return List.from(listDirectory)..addAll(listFile);
   }
 }
