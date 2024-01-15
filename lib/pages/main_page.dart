b724efc6133d67fb98a375b6da45c32bc5a77583
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-01-15 13:15:33 -0300

    customizing repository view bar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'dart:io' as io;
+
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mindcode_interface/pages/initial_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key, required this.path, required this.listDirectory});
@@ -19,35 +24,129 @@ class _MainPageState extends State<MainPage> {
         mainAxisSize: MainAxisSize.max,
         children: [
           Container(
-            width: MediaQuery.of(context).size.width * 0.2,
+            width: MediaQuery.of(context).size.width * 0.15,
             decoration: const BoxDecoration(
+              color: Color(0xFF0A0A16),
               border: Border(
                 right: BorderSide(
                   width: 1,
                   color: Colors.black,
                 ),
               ),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  blurRadius: 60,
+                  color: Color(0xFFE2ECF9),
+                  offset: Offset(0, 0.60),
+                )
+              ],
             ),
-            child: ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: widget.listDirectory.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(top: 7),
-                  child: Container(
-                    width: 120,
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                      width: 1,
-                      color: Colors.black,
-                    )),
-                    child: Center(
-                      child: Text(
-                          '${widget.listDirectory[index].path.replaceAll('\\', '/').split('/').last}'),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(
+                      top: 25, bottom: 25, left: 0, right: 0),
+                  width: MediaQuery.of(context).size.width * 0.2,
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        width: 2,
+                        color: Color(0xFFC3B4FF),
+                      ),
                     ),
                   ),
-                );
-              },
+                  child: Text('MindCode',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        color: const Color(0xFFD1C6FF),
+                        fontSize: 23,
+                        fontWeight: FontWeight.w600,
+                      )),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(25),
+                  width: MediaQuery.of(context).size.width * 0.2,
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Color(0xFFC3B4FF),
+                      ),
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const InitialPage(),
+                          ),
+                          (route) => false);
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: const MaterialStatePropertyAll(
+                        Color(0xff473f74),
+                      ),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                      ),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 10),
+                          child: Icon(
+                            Icons.search,
+                          ),
+                        ),
+                        Text(
+                          'Search File',
+                          style: GoogleFonts.poppins(
+                            color: const Color(0xFFD1C6FF),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    padding: const EdgeInsets.all(8),
+                    itemCount: widget.listDirectory.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 7),
+                        child: Container(
+                          width: 120,
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            width: 1,
+                            color: Colors.black,
+                          )),
+                          child: Center(
+                            child: Text(
+                              widget.listDirectory[index].path
+                                  .replaceAll('\\', '/')
+                                  .split('/')
+                                  .last,
+                              style: GoogleFonts.poppins(
+                                color: const Color(
+                                  0xFFD1C6FF,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
           Expanded(
